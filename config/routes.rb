7e6ca3d3e5c00b01c0aac9a17c4ef06e6d4db323
Kevin Exposito <kevin@mimiquate.com>
2021-03-04 15:11:36 -0300

    Iteration J2: Authenticating Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
   resources :orders
   resources :line_items
@@ -9,3 +16,4 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
